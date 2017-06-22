e03c6c8a8fe539cafa7eddfa68a20a59b3f6541d
Alessandro Feitoza <eu@alessandrofeitoza.eu>
2017-06-22 09:38:02 -0300

    improvement: returning true or false

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # zenvia
+
+
+### How to use
+
+```php
+    use FeitozaAle\Zenvia;
+
+    $sms = new Zenvia('conta', 'senha-integração');
+    $sms->send('5585987654321', 'Ande tooooooooonha);
+```
+
+
--- src/Zenvia.php
+++ src/Zenvia.php
@@ -48,17 +48,16 @@ class Zenvia
             //Envia a mensagem para o webservice e retorna um objeto do tipo SmsResponse com o status da mensagem enviada
             $response = $smsFacade->send($sms);
 
-            echo "Status: ".$response->getStatusCode() . " - " . $response->getStatusDescription();
-            echo "\nDetalhe: ".$response->getDetailCode() . " - " . $response->getDetailDescription();
-
-            if($response->getStatusCode()!="00"){
-                echo "\nMensagem não pôde ser enviada.";
-            }
-
         }
         catch(\Exception $ex){
-            echo "Falha ao fazer o envio da mensagem. Exceção: ".$ex->getMessage()."\n".$ex->getTraceAsString();
+            return false;
         }
+
+        if($response->getStatusCode()!="00"){
+            return false;
+        }
+
+        return true;
 
     }
 
